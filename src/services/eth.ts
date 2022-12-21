0c0f0e899dcc3d7f3438eefd63c9cca6fd3f52ad
Vlad Faust <hey@vladfaust.com>
2022-12-22 02:57:39 +0400

    feat: wss eth rpc

--- src/services/eth.ts
+++ src/services/eth.ts
@@ -7,9 +7,18 @@ import {
   NFTFair__factory,
 } from "@fancysoft/contracts/typechain";
 
+let provider: ethers.providers.BaseProvider;
+
 console.log("Connecting to JSON-RPC provider at", config.eth.rpcUrl);
-export const provider = new ethers.providers.JsonRpcProvider(config.eth.rpcUrl);
+if (config.eth.rpcUrl.startsWith("ws")) {
+  provider = new ethers.providers.WebSocketProvider(config.eth.rpcUrl);
+} else {
+  provider = new ethers.providers.JsonRpcProvider(config.eth.rpcUrl);
+}
+
 await timeout(5000, provider.ready, "JSON-RPC provider not ready");
+
+export { provider };
 
 export const talentContract = IPNFT1155Redeemable__factory.connect(
   config.eth.talentAddress.toString(),
